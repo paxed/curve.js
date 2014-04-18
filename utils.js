0de46a7a0ac06cc037a59053fc7efb94b9658f1b
Pasi Kallinen <paxed@alt.org>
2014-04-18 22:34:41 +0300

    Save selected textures into a cookie

--- utils.js
+++ utils.js
@@ -1,2 +1,23 @@
 
 function $(e) { return document.getElementById(e); }
+
+function setcookie(name,value,expiredays) {
+    var d = new Date();
+    d.setTime(d.getTime()+(expiredays*24*60*60*1000));
+    var expires = "expires="+d.toGMTString();
+    document.cookie = name + "=" + value + "; " + expires;
+}
+
+function getcookie(name) {
+    var n = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0; i < ca.length; i++) {
+	var c = ca[i].trim();
+	if (c.indexOf(name)==0) return c.substring(n.length,c.length);
+    }
+    return "";
+}
+
+function erasecookie(name) {
+    setcookie(name,"",-1);
+}
